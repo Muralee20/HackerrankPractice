9695ee74494ca23947e049ab8647db7aff0c9500
Muralee20 <30544293+Muralee20@users.noreply.github.com>
2019-01-03 21:21:48 -0800

    its correct to executed with me...

--- Python/09. Errors and Exceptions/002. Incorrect Regex.py
+++ Python/09. Errors and Exceptions/002. Incorrect Regex.py
@@ -1,12 +1,9 @@
-# Problem: https://www.hackerrank.com/challenges/incorrect-regex/problem
-# Score: 20
-
-
 import re
-
-
-for _ in range(int(input())):
+for i in range(int(raw_input())):
     try:
-        print(bool(re.compile(input())))
-    except re.error:
-        print('False')
+        s = raw_input()
+        re.compile(s)
+    except:
+        print False
+        continue
+    print True   
